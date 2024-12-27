f9f0a9ee38eac7639785bd9d4609cc96902b96ff
Mayun-Binod <shresthabinod334@gmail.com>
2024-12-27 20:02:38 +0545

    interacting with models and forms

--- tweetapp/admin.py
+++ tweetapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from tweetapp.models import Tweet
 # Register your models here.
+
+@admin.register(Tweet)
+class TweetAdmin(admin.ModelAdmin):
+    list_display=['user','text','photo','created_at','updated_at']
--- tweetapp/views.py
+++ tweetapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from .models import Tweet
+from .forms import TweetForm 
+from django.shortcuts import get_object_or_404
 # Create your views here.
 def index(request):
     return render(request,"tweetapp/index.html")
